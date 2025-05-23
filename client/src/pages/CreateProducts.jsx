71c5b956a8585760d75764e223cd79f714a0e6f5
Kotla Lokeshwari <kotlalokeshwari1871@gmail.com>
2025-05-23 14:06:07 +0530

    handling the createProducts form

--- client/src/pages/CreateProducts.jsx
+++ client/src/pages/CreateProducts.jsx
@@ -1,6 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-function createProducts() {
+function CreateProducts() {
+
+    const [form,setFormData]= useState({
+         image:'',
+         name:'',
+         price:'',
+         discount:'',
+         bgcolor:'',
+         textcolor:'',
+         panelcolor:''
+
+    })
+    const [products,setProducts]=useState([]);
+    function handleChange(e){
+       setFormData(prev=>{
+        return {
+         
+               ...prev,
+                [e.target.name]:e.target.value
+         
+        }
+       })
+    }
+
+    function handleSubmit(e){
+        setProducts(prev=>[...prev,form])
+        e.preventDefault()
+        setFormData({
+            image:'',
+         name:'',
+         price:'',
+         discount:'',
+         bgcolor:'',
+         textcolor:'',
+         panelcolor:''
+        })
+    } 
+    useEffect(()=>{
+   
+        console.log(products)
+    },[products])
   return (
     <div>
       <div class="min-h-screen flex flex-col">
@@ -13,20 +53,20 @@ function createProducts() {
                     </div>
                     <main class="w-3/4 bg-white p-8 shadow ml-4">
                         <h2 class="text-xl font-bold mb-4">Create New Product</h2>
-                        <form autocomplete="off" action="/products/create" method="post" enctype="multipart/form-data">
+                        <form autocomplete="off" action="" method="" enctype="multipart/form-data" onSubmit={handleSubmit}>
                             <div class="mb-6">
                                 <h3 class="text-lg font-semibold mb-2">Product Details</h3>
                                 <div class="mb-4">
                                     <label class="block mb-2 font-medium">Product Image</label>
-                                    <input name="image" type="file" class="py-2 px-4 rounded" value="Select File"/>
+                                    <input name="image" type="file" class="py-2 px-4 rounded" value={form.image} onChange={handleChange}/>
                                 </div>
                                 <div class="grid grid-cols-2 gap-4">
                                     <input name="name" type="text" placeholder="Product Name"
-                                        class="border p-2 rounded w-full"/>
+                                        class="border p-2 rounded w-full" value={form.name} onChange={handleChange}/>
                                     <input name="price" type="text" placeholder="Product Price"
-                                        class="border p-2 rounded w-full"/>
+                                        class="border p-2 rounded w-full" value={form.price} onChange={handleChange}/>
                                     <input name="discount" type="text" placeholder="Discount Price"
-                                        class="border p-2 rounded w-full"/>
+                                        class="border p-2 rounded w-full" value={form.discount} onChange={handleChange}/>
                                 </div>
                             </div>
 
@@ -34,11 +74,11 @@ function createProducts() {
                                 <h3 class="text-lg font-semibold mb-2">Panel Details</h3>
                                 <div class="grid grid-cols-2 gap-4">
                                     <input name="bgcolor" type="text" placeholder="Background Color"
-                                        class="border p-2 rounded w-full"/>
+                                        class="border p-2 rounded w-full" value={form.bgcolor} onChange={handleChange}/>
                                     <input name="panelcolor" type="text" placeholder="Panel Color"
-                                        class="border p-2 rounded w-full"/>
+                                        class="border p-2 rounded w-full" value={form.panelcolor} onChange={handleChange}/>
                                     <input name="textcolor" type="text" placeholder="Text Color"
-                                        class="border p-2 rounded w-full"/>
+                                        class="border p-2 rounded w-full" value={form.textcolor} onChange={handleChange}/>
                                 </div>
                             </div>
                             <input class="px-5 py-2 rounded mt-3 bg-blue-500 text-white" type="submit"
@@ -51,4 +91,4 @@ function createProducts() {
   )
 }
 
-export default createProducts
+export default CreateProducts
